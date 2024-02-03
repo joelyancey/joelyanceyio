91852c563dbba194710b16cc2baec1c68bc0e19d
Joel Yancey <j.y@ucla.edu>
2024-02-02 23:15:51 -0800

    Update some more text

--- src/layouts/Main.js
+++ src/layouts/Main.js
@@ -11,7 +11,7 @@ const Main = (props) => (
   <HelmetProvider>
     <Analytics />
     <ScrollToTop />
-    <Helmet titleTemplate="%s | Michael D'Angelo" defaultTitle="Michael D'Angelo" defer={false}>
+    <Helmet titleTemplate="%s | Joel Yancey" defaultTitle="Joel Yancey" defer={false}>
       {props.title && <title>{props.title}</title>}
       <meta name="description" content={props.description} />
     </Helmet>
